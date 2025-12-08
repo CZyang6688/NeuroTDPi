443575b257e4ccf6f7168cc125d46ed00f477f24
chen zhaoyang <140325186+CZyang6688@users.noreply.github.com>
2025-12-09 00:44:45 +0800

    Update NC_predict.py

--- predict/NC_predict.py
+++ predict/NC_predict.py
@@ -4,15 +4,15 @@ from sklearn import metrics
 import pandas as pd
 
 #加载数据集
-df_x_test =pd.read_csv("D:\Code\Neuron\BBB\BBB_jingjian\BBB理化性质test-x.csv", na_values=["?", "NA"])
-df_y_test = pd.read_csv("D:\Code\Neuron\BBB\BBB_jingjian\BBB理化性质test-y.csv", na_values=["?", "NA"])
+df_x_test =pd.read_csv("data/NC_test-x.csv", na_values=["?", "NA"])
+df_y_test = pd.read_csv("data/NC_test-y.csv", na_values=["?", "NA"])
 df_test = pd.concat([df_x_test, df_y_test], axis=1)
 clean_data_test = df_test.dropna()
 x_test = clean_data_test.iloc[:, :-1].to_numpy()
 y_test = clean_data_test.iloc[:, -1].to_numpy()
 
 # 加载模型
-model = load_model('/Neuron/BBB/BBB_jingjian/model_BBB理化性质-精简.h5')
+model = load_model('model/NC_predict.h5')
 
 # 使用模型预测测试集
 y_pred = model.predict(x_test)
@@ -51,4 +51,5 @@ print("AUC:", auc)
 print("Sensitivity (SE):", se)
 print("Specificity (SP):", sp)
 print("MCC:", C)
+
 print("F1-SCORE:", f_measure)
